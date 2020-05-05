7d160ccbf6141233581f7c4bb33a0455d3737c33
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-05-05 15:07:50 -0300

    Feat: create Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        Logged in.
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
