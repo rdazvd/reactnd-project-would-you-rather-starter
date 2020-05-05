10c945456e8d27f8647849aed845d1a224aa4700
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-05-05 16:19:33 -0300

    Feat: create _getUser method in raw data file

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -119,6 +119,12 @@ function generateUID () {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
 
+export function _getUser(id) {
+  return new Promise((res, rej) => {
+      setTimeout(() => res(users[id]), 1000)
+  });
+}
+
 export function _getUsers () {
   return new Promise((res, rej) => {
     setTimeout(() => res({...users}), 1000)
