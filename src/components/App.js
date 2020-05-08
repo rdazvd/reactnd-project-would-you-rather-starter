bd45ca457a34cfbe6c676c0a2affdcbc5772fe46
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-05-08 17:49:14 -0300

    Refactor: turn App into a class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      app starter
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        app starter
+      </div>
+    );
+  } 
 }
-
-export default App;
