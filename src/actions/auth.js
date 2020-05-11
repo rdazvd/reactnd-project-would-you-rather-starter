49e05e4c3d9cc49dbf2782c47db4df617b94a2cb
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-05-11 18:06:36 -0300

    Refactor: export expressions in auth actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,22 +1,22 @@
 import { getUser } from '../utils/api';
 import { showLoading, hideLoading } from 'react-redux-loading';
 
-const AUTH_LOGIN_SUCCESS = 'AUTH_LOGIN_SUCCESS'; 
-const AUTH_LOGOUT_SUCCESS = 'AUTH_LOGOUT_SUCCESS';
+export const AUTH_LOGIN_SUCCESS = 'AUTH_LOGIN_SUCCESS'; 
+export const AUTH_LOGOUT_SUCCESS = 'AUTH_LOGOUT_SUCCESS';
 
-const receiveAuthLogin = user => ({
+export const receiveAuthLogin = user => ({
   type: AUTH_LOGIN_SUCCESS,
   userAuthenticated: true,
   loggedInUser: user
 });
 
-const receiveAuthLogout = () => ({
+export const receiveAuthLogout = () => ({
   type: AUTH_LOGOUT_SUCCESS,
   userAuthenticated: false,
   loggedInUser: null
 });
 
-const handleLoginUser = id =>
+export const handleLoginUser = id =>
   dispatch => {
     dispatch(showLoading());
     getUser(id).then(user => {
@@ -25,17 +25,9 @@ const handleLoginUser = id =>
     });
   };
 
-const handleLogoutUser = () =>
+export const handleLogoutUser = () =>
   dispatch => {
     dispatch(showLoading());
     dispatch(receiveAuthLogout());
     dispatch(hideLoading());
   };
-
-export {
-  AUTH_LOGIN_SUCCESS,
-  AUTH_LOGOUT_SUCCESS,
-  handleLoginUser,
-  handleLogoutUser,
-  receiveAuthLogout
-};
