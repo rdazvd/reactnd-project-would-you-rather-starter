c25b50797e277c1a0e82c027d1f3f31fe522d0ac
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-05-11 18:25:26 -0300

    Feat: create questions reducer

---
+++ src/reducers/questions.js
@@ -0,0 +1,31 @@
+import { RECEIVE_QUESTIONS, ADD_QUESTION, ADD_ANSWER } from '../actions/questions';
+
+const questions = ({ state = {}, action }) => {
+  switch(action.type) {
+    case RECEIVE_QUESTIONS:
+      return {
+        ...state,
+        ...action.questions
+      };
+    case ADD_QUESTION:
+      return {
+        ...state,
+        [action.question.id]: action.question
+      };
+    case ADD_ANSWER:
+      return {
+        ...state,
+        [action.questionId]: {
+          ...state[action.questionId],
+          [action.selectedOption]: {
+            ...state[action.questionId][action.selectedOption],
+            votes: state[action.questionId][action.selectedOption].votes.concat([action.authedUser])
+          }
+        }
+      };
+    default:
+      return state;
+  }
+}
+
+export default questions;
