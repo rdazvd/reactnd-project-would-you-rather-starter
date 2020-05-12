46a803106fb9a09b9c58530ddaa711bdd4ef869f
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-05-12 17:01:12 -0300

    Feat: create user question related actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,14 +1,16 @@
 import { getUsers } from '../utils/api';
 import { showLoading, hideLoading } from 'react-redux-loading';
 
-const RECEIVE_USERS = 'RECEIVE_USERS';
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const ADD_USER_QUESTION = 'ADD_USER_QUESTION';
+export const ADD_USER_ANSWER = 'ADD_USER_ANSWER';
 
-const receiveUsers = users => ({
+export const receiveUsers = users => ({
   type: RECEIVE_USERS,
   users
 });
 
-const handleGetUsers = () =>
+export const handleGetUsers = () =>
   dispatch => {
     dispatch(showLoading());
     return getUsers()
@@ -18,8 +20,14 @@ const handleGetUsers = () =>
       });
   };
 
-export {
-  RECEIVE_USERS,
-  receiveUsers,
-  handleGetUsers
-};
+  export const addUserQuestion = question => ({
+    type: ADD_USER_QUESTION,
+    question
+  });
+
+  export const addUserAnswer = (authedUser, questionId, selectedOption) => ({
+    type: ADD_USER_ANSWER,
+    authedUser,
+    questionId,
+    selectedOption
+  });
