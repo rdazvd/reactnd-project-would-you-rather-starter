6842a947067dca4e4be17cd126cd59dae859a711
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-05-15 18:33:06 -0300

    Feat: add styling to NavBar (ongoing)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import { 
+  Button,
+  Col,
+  Container,
+  Row
+ } from 'react-bootstrap';
 
 const NavBar = ({loggedInUser}) => (
   <div>
-    <nav>
-      <ul>
-        <NavLink exact to='/'>Home</NavLink>
-        <NavLink exact to='/add'>New Question</NavLink>
-      </ul>
+    <nav className='navbar navbar-expand navbar-light bg-light'>
+      <Container className='collapse navbar-collapse'>
+        <Row className='justify-content-between' style={{ width: '100vw' }}>
+          <Col xs='auto'>
+            <ul className='navbar-nav'>
+              <NavLink className='nav-item nav-link' exact to='/'>Home</NavLink>
+              <NavLink className='nav-item nav-link' exact to='/add'>New Question</NavLink>
+              <NavLink className='nav-item nav-link' exact to='/add'>Leaderboard</NavLink>
+            </ul>
+          </Col>
+          <Col xs='auto'>
+            <span className='navbar-text text-info'>Hello {loggedInUser.name}</span>
+            <span className='navbar-item'><Link to='/logout'>Logout</Link></span>
+          </Col>
+        </Row>
+      </Container>
     </nav>
-    <Link to='/logout'><button>Logout</button></Link>
   </div>
 );
 
