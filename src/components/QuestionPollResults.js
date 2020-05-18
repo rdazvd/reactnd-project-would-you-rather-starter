f519ea339759e603f22e97049ab386a41a1a6b76
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-05-18 17:19:42 -0300

    Feat: styling added to QuestionPollResults

--- src/components/QuestionPollResults.js
+++ src/components/QuestionPollResults.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { 
+  Card,
+  Col,
+  Container,
+  Image,
+  Row,
+  ProgressBar
+} from 'react-bootstrap';
 import PageNotFound from './PageNotFound';
 
 const QuestionPollResults = ({ question, author, pageNotFound }) => {
@@ -17,24 +25,50 @@ const QuestionPollResults = ({ question, author, pageNotFound }) => {
   const optionTwoWidth = Math.round((optionTwoVotes.length / totalVotes) * 100);
 
   return (
-    <div>
-      <div>Added by {author.name}</div>
-      <div>
-        <img src={author.avatarURL} alt={`Avatar of ${author.name}`} />
-      </div>
-      <div>
-        <p>Would you rather {question.optionOne.text}?</p>
-        <div>
-          <div style={{ width: `${optionOneWidth}%` }}></div>
-        </div>
-      </div>
-      <div>
-        <p>Would you rather {question.optionTwo.text}?</p>
-        <div>
-          <div style={{ width: `${optionTwoWidth}%` }}></div>
-        </div>
-      </div>
-    </div>
+    <Container className='mt-5'>
+      <Row className='justify-content-center'>
+        <Card className='p-5' style={{ width: '60%' }}>
+          <Card.Title>Added by {author.name}</Card.Title>
+          <Container>
+            <Row>
+              <Col xs={2}>
+                <Image
+                  src={author.avatarURL}
+                  alt={`Avatar of ${author.name}`}
+                  roundedCircle
+                  style={{
+                    height: '5rem',
+                    width: '5rem'
+                  }}
+                />
+              </Col>
+              <Col xs={6}>
+                <Card.Body>
+                  <div className='mb-4'>
+                    <p>Would you rather {question.optionOne.text}?</p>
+                    <ProgressBar 
+                      now={optionOneWidth}
+                      variant={optionSelected === 'optionOne' ? 'success' : 'info'}
+                      label={optionSelected === 'optionOne' ? 'Chosen answer': ''} 
+                    />
+                    <p>{optionOneVotes.length} out of {totalVotes} votes.</p>
+                  </div>
+                  <div>
+                    <p>Would you rather {question.optionTwo.text}?</p>
+                    <ProgressBar
+                      now={optionTwoWidth}
+                      variant={optionSelected === 'optionTwo' ? 'success' : 'info'}
+                      label={optionSelected === 'optionTwo' ? 'Chosen answer': ''} 
+                    />
+                    <p>{optionTwoVotes.length} out of {totalVotes} votes.</p>
+                  </div>                  
+                </Card.Body>
+              </Col>
+            </Row>
+          </Container>
+        </Card>
+      </Row>
+    </Container>
   );
 };
 
