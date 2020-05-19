763229f54d614acf40755942185e9af696a0f96a
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-05-19 11:34:07 -0300

    Refactor: remake QuestionPollResults as a class This commit improves state handling whithin the QuestionPollResults component, as its lifecycle was generating glitches on the styling of the progress bars.

--- src/components/QuestionPollResults.js
+++ src/components/QuestionPollResults.js
@@ -10,66 +10,98 @@ import {
 } from 'react-bootstrap';
 import PageNotFound from './PageNotFound';
 
-const QuestionPollResults = ({ question, author, pageNotFound }) => {
-  if (pageNotFound) return <PageNotFound />;
+class QuestionPollResults extends React.Component {
+  state = {
+    optionOneVariant: '',
+    optionOneLabel: '',
+    optionTwoVariant: '',
+    optionTwoLabel: ''
+  };
 
-  const optionOneVotes = question.optionOne.votes;
-  const optionTwoVotes = question.optionTwo.votes;
+  componentDidMount() {
+    const { author, question } = this.props;
+    const questionOneVotes = question.optionOne.votes;
+    const questionTwoVotes = question.optionTwo.votes;
 
-  const totalVotes = optionOneVotes.length + optionTwoVotes.length;
-  const optionSelected = optionOneVotes.includes(author.id)
-    ? 'optionOne'
-    : 'optionTwo';
+    this.setState({
+      optionOneVariant: questionOneVotes.includes(author.id)
+        ? 'success'
+        : 'info',
+      optionOneLabel: questionOneVotes.includes(author.id)
+        ? 'Chosen Answer'
+        : '',
+      optionTwoVariant: questionTwoVotes.includes(author.id)
+        ? 'success'
+        : 'info',
+      optionTwoLabel: questionTwoVotes.includes(author.id)
+        ? 'Chosen Answer'
+        : '',
+    });
+  }
 
-  const optionOneWidth = Math.round((optionOneVotes.length / totalVotes) * 100);
-  const optionTwoWidth = Math.round((optionTwoVotes.length / totalVotes) * 100);
-
-  return (
-    <Container className='mt-5'>
-      <Row className='justify-content-center'>
-        <Card className='p-5' style={{ width: '60%' }}>
-          <Card.Title>Added by {author.name}</Card.Title>
-          <Container>
-            <Row>
-              <Col xs={2}>
-                <Image
-                  src={author.avatarURL}
-                  alt={`Avatar of ${author.name}`}
-                  roundedCircle
-                  style={{
-                    height: '5rem',
-                    width: '5rem'
-                  }}
-                />
-              </Col>
-              <Col xs={6}>
-                <Card.Body>
-                  <div className='mb-4'>
-                    <p>Would you rather {question.optionOne.text}?</p>
-                    <ProgressBar 
-                      now={optionOneWidth}
-                      variant={optionSelected === 'optionOne' ? 'success' : 'info'}
-                      label={optionSelected === 'optionOne' ? 'Chosen answer': ''} 
-                    />
-                    <p>{optionOneVotes.length} out of {totalVotes} votes.</p>
-                  </div>
-                  <div>
-                    <p>Would you rather {question.optionTwo.text}?</p>
-                    <ProgressBar
-                      now={optionTwoWidth}
-                      variant={optionSelected === 'optionTwo' ? 'success' : 'info'}
-                      label={optionSelected === 'optionTwo' ? 'Chosen answer': ''} 
-                    />
-                    <p>{optionTwoVotes.length} out of {totalVotes} votes.</p>
-                  </div>                  
-                </Card.Body>
-              </Col>
-            </Row>
-          </Container>
-        </Card>
-      </Row>
-    </Container>
-  );
+  render() {
+    const { question, author, pageNotFound } = this.props;
+    
+    if (pageNotFound) return <PageNotFound />;
+  
+    const optionOneVotes = question.optionOne.votes;
+    const optionTwoVotes = question.optionTwo.votes;
+  
+    const totalVotes = optionOneVotes.length + optionTwoVotes.length;
+    const optionSelected = optionOneVotes.includes(author.id)
+      ? 'optionOne'
+      : 'optionTwo';
+  
+    const optionOneWidth = Math.round((optionOneVotes.length / totalVotes) * 100);
+    const optionTwoWidth = Math.round((optionTwoVotes.length / totalVotes) * 100);
+  
+    return (
+      <Container className='mt-5'>
+        <Row className='justify-content-center'>
+          <Card className='p-5' style={{ width: '60%' }}>
+            <Card.Title>Added by {author.name}</Card.Title>
+            <Container>
+              <Row>
+                <Col xs={2}>
+                  <Image
+                    src={author.avatarURL}
+                    alt={`Avatar of ${author.name}`}
+                    roundedCircle
+                    style={{
+                      height: '5rem',
+                      width: '5rem'
+                    }}
+                  />
+                </Col>
+                <Col xs={6}>
+                  <Card.Body>
+                    <div className='mb-4'>
+                      <p>Would you rather {question.optionOne.text}?</p>
+                      <ProgressBar 
+                        now={optionOneWidth}
+                        variant={optionSelected === 'optionOne' ? 'success' : 'info'}
+                        label={optionSelected === 'optionOne' ? 'Chosen answer': ''} 
+                      />
+                      <p>{optionOneVotes.length} out of {totalVotes} votes.</p>
+                    </div>
+                    <div>
+                      <p>Would you rather {question.optionTwo.text}?</p>
+                      <ProgressBar
+                        now={optionTwoWidth}
+                        variant={optionSelected === 'optionTwo' ? 'success' : 'info'}
+                        label={optionSelected === 'optionTwo' ? 'Chosen answer': ''} 
+                      />
+                      <p>{optionTwoVotes.length} out of {totalVotes} votes.</p>
+                    </div>                  
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Container>
+          </Card>
+        </Row>
+      </Container>
+    );
+  }
 };
 
 const mapStateToProps = ({questions, users} , props) => {
